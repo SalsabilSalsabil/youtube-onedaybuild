58f62b531156b151ee620e6be0a49411b6e3cfa0
salsabil dablij <salsabil.dablij@babelgroup.com>
2024-11-07 11:39:21 +0100

    initial Xcode project creation

---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by Salsabi Dablij on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
